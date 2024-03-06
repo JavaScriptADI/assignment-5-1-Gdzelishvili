92277e5ae4e8080ef3d36f3cbebc0532fe5541f4
Lasha <grdzelishvili.lasha@yahoo.com>
2024-03-06 17:40:22 +0400

    finished firs task

--- task.js
+++ task.js
@@ -3,7 +3,7 @@ const PRODUCT_COUNT = 50;
 const products = [];
 
 // Generate Products
-// ეს ფუნქცია იღებს 2 პარამეტრს მინიმუმ ციპრს და მაქსიმუმს მათ შორის იძლევა რანდომ
+// ეს ფუნქცია იღებს 2 პარამეტრს მინიმუმ ციფრს და მაქსიმუმს მათ შორის იძლევა რანდომ
 // ციპრს
 
 
@@ -23,9 +23,23 @@ function getRandomString(length) {
     return text;
 }
 // ეს კი 50 პროდუქტს უმატებს რანდომ სტრინგების სახით ერეის
+
 for (let i = 0; i < PRODUCT_COUNT; i++)
     products.push({
         title: getRandomString(getRandomInt(30, 60)),
         price: getRandomInt(500, 5000),
         description: getRandomString(getRandomInt(300, 1000))
     })
+
+    console.log(products);
+
+    // 1. count average price
+
+
+
+const totalPrice = products.reduce((acc, product) => acc + product.price, 0);
+
+
+const averagePrice = totalPrice / products.length;
+
+console.log(averagePrice);
